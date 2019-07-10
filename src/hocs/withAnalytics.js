d1358b902075015c644b4eb5b159d35fa861b103
Thiago Amâncio <tjamancio@censanet.com.br>
2019-07-10 11:36:47 -0300

    Initial Commit

---
+++ src/hocs/withAnalytics.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import ReactGA from 'react-ga';
+
+//HOC
+export default () => Composed =>
+  class extends Component {
+    static getInitialProps(ctx) {
+      if (Composed.getInitialProps) {
+        return Composed.getInitialProps(ctx);
+      }
+
+      return {};
+    }
+
+    componentDidMount() {
+      ReactGA.initialize('ID_ANALYTICS');
+      ReactGA.pageview(window.location.pathname);
+    }
+
+    render() {
+      return <Composed {...this.props} />;
+    }
+  };
